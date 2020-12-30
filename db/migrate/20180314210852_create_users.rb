db28b3c8bb5b6238ba9f3a3cc94c0643b75a2924
elijahsamuels <elijahsamuels@gmail.com>
2020-12-30 11:30:02 -0700

    commit tree c61a30ab8fc4b706e6ed1b6f8181f97a1abbf237

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -1,7 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
-  def up
-  end
   
-  def down
+  def up
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+    end
   end
-end
+
+  def down
+    drop_table :users
+  end
+end 
